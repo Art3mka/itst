20bfc03301124036c524d0ffe8563db3017d6749
RTQ <tema34468@gmail.com>
2025-09-18 20:17:39 +0300

    fix .dockerignore file

--- config/db.php
+++ config/db.php
@@ -2,10 +2,11 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=mini_blog',
+    'dsn' => 'mysql:host=db;dbname=mini_blog', //Для запуска Docker контейнера
+    // 'dsn' => 'mysql:host=localhost;dbname=mini_blog',
     'username' => 'root',
-    // 'password' => 'rootpassword', Для запуска Docker контейнера
-    'password' => '',
+    'password' => 'rootpassword', //Для запуска Docker контейнера
+    // 'password' => '',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
